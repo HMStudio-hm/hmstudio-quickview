4ebb5aa5f7be8af75b51eb22b63408bf8e12cd9b
HMStudio-hm <hamzaboudchicha3@gmail.com>
2024-10-16 21:19:42 +0100

    Update quickView.js

--- quickView.js
+++ quickView.js
@@ -15,12 +15,12 @@
   }
 
   function fetchSettings() {
-    const storeId = getStoreId();
-    if (!storeId) {
+    const managerId = getStoreId();
+    if (!managerId) {
       console.error('Store ID not found');
       return;
     }
-    fetch(`https://3cbe-105-156-116-5.ngrok-free.app/api/quick-view-settings?storeId=${storeId}`)
+    fetch(`https://3cbe-105-156-116-5.ngrok-free.app/api/quick-view-settings?storeId=${managerId}`)
       .then(response => response.json())
       .then(settings => {
         currentSettings = settings;
